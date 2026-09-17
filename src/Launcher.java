28dbc76034ce14b15bc332af3491de76d41fab4f
Sinammon <sinanmakkadayil@gmail.com>
2026-09-16 23:05:05 -0400

    Restore JavaFX dashboard layout

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,7 +1,5 @@
-import javafx.application.Application;
-
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Main.class, args);
+        Main.main(args);
     }
 }
